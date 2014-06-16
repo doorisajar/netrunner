23cdfd3d237b6cba4b1a0250cf22b9b599a8ab82
Raja Doake <rdoake@gmail.com>
2014-06-15 19:44:57 -0500

    download.octgn() works, but via a hard link.

--- R/download.octgn.R
+++ R/download.octgn.R
@@ -1,35 +1,50 @@
 #' Read in an Android: Netrunner OCTGN data file. 
 #'
-#' This function downloads the latest Android: Netrunner OCTGN data file from http://octgn.gamersjudgment.net/
+#' This function downloads the latest Android: Netrunner OCTGN .csv data file from http://octgn.gamersjudgment.net/
 #' 
 #' @param quietly If TRUE, don't display any messages or a progress bar while downloading.  
+#' @return octgn A data frame read from the downloaded .csv. 
 #' @import httr stringr
 #' 
 #' @export
 
 download.octgn <- function( quietly = TRUE ) {
   
-  page <- content( GET( url = "http://octgn.gamersjudgement.com/", path = "wordpress/anr/statistics/" ), 
-                   as = "parsed",
-                   type = "text/html" )
+  octgn <- content( GET( url = "https://doc-00-4o-docs.googleusercontent.com/",
+                         path = "docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/v8rqrt2sp9hcbc2fqhjrstutsusqv19g/1402804800000/07708637904532248706/*/0B-gMiPlH3rBANWVVZXR5OTBENlU?h=16653014193614665626&e=download"
+                        ), 
+                    type = "text/csv"
+                  ) 
   
-  page <- getHTMLLinks( page )
+# What follows is a bunch of messing about trying to automatically extract the file ID from db0's site
+# (which works) so that I can always download the latest file from Google Drive (which does not work).
+#
+# Unclear why OAuth is needed to download a public file. 
   
-  page <- page[which( str_detect( page, "drive.google.com" ) )]
-  
-#  path <- substr( page, start = 25, stop = nchar( page ) )  
-  path <- unlist( strsplit( page, "file" ) )[2]
-  path <- unlist( strsplit( path, "edit" ) )[1]
-  
-#   Got a token, but the official way still throws a 404. ??? 
-
-  path <- substr( path, start = 4, stop = nchar( path ) - 1 ) 
-  fields <- "?fields=downloadUrl"
-  key <- "?key=ya29.LgAHpoOw8iYIdSAAAAAOzgpFvdkOBWvhF_pytIT_H3nk4xiSwR_UAUkc2skzTA"
-  octgn <- GET( url = "https://www.googleapis.com/drive/v2/files/", 
-                path = paste0(path, fields, "&", key ) )  
-
-
+#   page <- content( GET( url = "http://octgn.gamersjudgement.com/", path = "wordpress/anr/statistics/" ), 
+#                    as = "parsed",
+#                    type = "text/html" )
+#   
+#   page <- getHTMLLinks( page )
+#   
+#   page <- page[which( str_detect( page, "drive.google.com" ) )]
+#   
+# #  path <- substr( page, start = 25, stop = nchar( page ) )  
+#   path <- unlist( strsplit( page, "file" ) )[2]
+#   path <- unlist( strsplit( path, "edit" ) )[1]
+#   
+# #   Got a token, but the official way still throws a 404. ??? 
+# 
+#   path <- substr( path, start = 4, stop = nchar( path ) - 1 ) 
+#   fields <- "downloadUrl"
+#   key <- "ya29.LgCCf6D6_mLm_SAAAAAamjyWJHRU698eTbHWEmEy8FpOzZIVoCUfDq47J7yE6Q"
+# 
+#   octgn <- GET( url = "https://www.googleapis.com/drive/v2/files/", 
+#                 path = path,
+#                 add_headers( fields = fields,
+#                              Authorization = key) )  
+# 
+# 
 
 
 #   octgn <- content( GET( url = "https://docs.google.com/", path = paste0( "file", path, "edit" ) ),
@@ -39,22 +54,19 @@ download.octgn <- function( quietly = TRUE ) {
 #
 #   "downloadUrl":"https://docs.google.com/uc?id\u003d0B-gMiPlH3rBANWVVZXR5OTBENlU\
 #   u0026export\u003ddownload\u0026revid\u003d0B-gMiPlH3rBAaGYyRmpKMm5uYmJrdVRaZHZBTTRlTDlVSExJPQ"
-
-  octgn <- str_extract( octgn, pattern = "(downloadUrl\":\")(.*?)\"" )
-
-  octgn <- substr( octgn, start = 39, stop = nchar( octgn ) - 1 )
-
-  test <- content( GET( url = "https://docs.google.com/", path = octgn ),
-                   type = "text/csv" )
+#
+#   octgn <- str_extract( octgn, pattern = "(downloadUrl\":\")(.*?)\"" )
+# 
+#   octgn <- substr( octgn, start = 39, stop = nchar( octgn ) - 1 )
+# 
+#   test <- content( GET( url = "https://docs.google.com/", path = octgn ),
+#                    type = "text/csv" )
 
 
 # "https://doc-14-bo-docs.googleusercontent.com/docs/securesc/o7l0aeqi0drljltr83c3565k9441n6of/fj2cr1s72ajlfidfqo88uc7lmvmimvvt/1402804800000/07708637904532248706/13058876669334088843/0B-gMiPlH3rBANWVVZXR5OTBENlU?h=16653014193614665626&e=download&gd=true"
 #  https://doc-00-4o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/v8rqrt2sp9hcbc2fqhjrstutsusqv19g/1402804800000/07708637904532248706/*/0B-gMiPlH3rBANWVVZXR5OTBENlU?h=16653014193614665626&e=download
 
-  test <- content( GET( url = "https://doc-00-4o-docs.googleusercontent.com/",
-                        path = "docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/v8rqrt2sp9hcbc2fqhjrstutsusqv19g/1402804800000/07708637904532248706/*/0B-gMiPlH3rBANWVVZXR5OTBENlU?h=16653014193614665626&e=download"
-                        ), 
-                   type = "text/csv"
-                   ) 
+
+return( octgn )
 
 }
